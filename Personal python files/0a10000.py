39db0d89951f57c56f64347b23d1b718b1d8e266
PedroRuama <ruama246@gmail.com>
2022-02-01 20:58:34 -0300

    excs e alterações

---
+++ Personal python files/0a10000.py
@@ -0,0 +1,13 @@
+from random import randint
+b = int(input("escolha um numero de 0 a 10000: "))
+c = 0
+d = 0
+while b != c:
+    a = randint(0,10000)
+    print(a)
+    d = d + 1
+    c = a
+print()
+print(d,"veses até acertar")
+   
+
---
+++ Personal python files/decimais.py
@@ -0,0 +1,28 @@
+""""
+arredondar x truncar
+
+Ao arredondar 3.19999 para 2 decimais o resultado é 3.20;
+
+Ao truncar3.19999 para 2 decimais o resultado é 3.19.
+"""
+
+#arredondado
+x = float(input("x: "  ))
+
+x = "%.2f" % x 
+#numero de casas decimas (se tiver mais casa do q o determinado, vai arredondoar)
+
+print(x)
+
+print()
+
+A = 3/9
+A = round(A,3)
+print (A)
+
+
+
+
+
+
+
